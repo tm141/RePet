94397af4ace0b9a1ea1656395bd8faf916da231b
Andre TM <andretimoti@yahoo.com>
2023-12-09 06:22:39 +0700

    refine font on home page

--- src/app/_ui/hero.tsx
+++ src/app/_ui/hero.tsx
@@ -22,9 +22,9 @@ export default function Hero() {
     return (
         <div className="py-10 desktop:py-24 align-middle flex flex-col desktop:flex-row px-[var(--px-sm)] desktop:px-[var(--px)] h-1/3 bg-[var(--color-fourth)] ">
             <div className="laptop:p-10 justify-center laptop:justify-normal">
-                <h1 className="px-5 py-2 w-fit rounded-lg text-xl laptop:text-4xl font-bold tracking-tight text-[var(--color-secondary)] bg-[var(--color-primary-2)] sm:text-6xl">RePet</h1>
-                <p className="mt-6 text-md text-bold laptop:text-2xl laptop:leading-8">Where Love Means no Surprises</p>
-                <p className="mt-6 text-sm laptop:text-md laptop:leading-8 laptop:text-justify">At RePet, we bring the joy of beloved pets back into your life through cutting-edge cloning techniques</p>
+                <h1 className="px-5 py-2 w-fit rounded-lg text-xl laptop:text-5xl font-bold tracking-tight text-[var(--color-secondary)] bg-[var(--color-primary-2)] sm:text-6xl">RePet</h1>
+                <p className="mt-6 text-md text-bold desktop:text-3xl laptop:leading-8">Where Love Means no Surprises</p>
+                <p className="leading-5 text-sm desktop:text-xl laptop:leading-6 laptop:tracking-wide pb-5 items-center mt-6 laptop:text-justify">At RePet, we bring the joy of beloved pets back into your life through cutting-edge cloning techniques</p>
             </div>
 
             <div className="min-w-[120px] max-w-[720px] desktop:min-w-[480px] mx-auto">
--- src/app/_ui/overView.tsx
+++ src/app/_ui/overView.tsx
@@ -10,16 +10,16 @@ export default function OverView() {
                 </div>
                 <div className="flex flex-col desktop:grid desktop:grid-rows-2 desktop:grid-cols-2 desktop:w-[50%] gap-4 ">
                     <div className="desktop:row-[1] desktop:leading-3  desktop:col-span-2 bg-[var(--color-fourth)] rounded-xl p-5 desktop:p-10">
-                        <h1 className="text-xl desktop:text-3xl font-bold mb-6">Our Vision</h1>
-                        <p className="tracking-wide desktop:leading-7">Our vision at rePet is to offer pet owners the unique opportunity to preserve the bond with their cherished pets, ensuring that the love, loyalty, and companionship they shared are perpetuated through advanced cloning technology. We strive to redefine the concept of pet companionship, transcending the boundaries of time and loss.</p>
+                        <h1 className="text-xl desktop:text-4xl font-bold mb-6">Our Vision</h1>
+                        <p className="leading-5 text-sm desktop:text-xl laptop:leading-6 laptop:tracking-wide pb-5 items-center">Our vision at rePet is to offer pet owners the unique opportunity to preserve the bond with their cherished pets, ensuring that the love, loyalty, and companionship they shared are perpetuated through advanced cloning technology. We strive to redefine the concept of pet companionship, transcending the boundaries of time and loss.</p>
                     </div>
                     <div className="desktop:row-[2] desktop:leading-3  desktop:col-[1] bg-[var(--color-fourth)] rounded-xl p-5">
-                        <h1 className="text-lg desktop:text-2xl font-bold mb-6">Ethical Cloning</h1>
-                        <p className="desktop:leading-7">We prioritize ethical considerations and transparency in our cloning process.</p>
+                        <h1 className="text-lg desktop:text-3xl font-bold mb-6">Ethical Cloning</h1>
+                        <p className="leading-5 text-sm desktop:text-xl laptop:leading-6 laptop:tracking-wide pb-5 items-center">We prioritize ethical considerations and transparency in our cloning process.</p>
                     </div>
                     <div className="desktop:row-[2] desktop:leading-3  desktop:col-[2] bg-[var(--color-fourth)] rounded-xl p-5">
-                        <h1 className="text-lg desktop:text-2xl font-bold mb-6">Responsible Cloning</h1>
-                        <p className="desktop:leading-7">Our practices adhere to all relevant regulations, ensuring the well-being of the cloned pets and promoting responsible cloning within the boundaries of ethical guidelines.</p>
+                        <h1 className="text-lg desktop:text-3xl font-bold mb-6">Responsible Cloning</h1>
+                        <p className="leading-5 text-sm desktop:text-xl laptop:leading-6 laptop:tracking-wide pb-5 items-center">Our practices adhere to all relevant regulations, ensuring the well-being of the cloned pets and promoting responsible cloning within the boundaries of ethical guidelines.</p>
                     </div>
                 </div>
             </div>
